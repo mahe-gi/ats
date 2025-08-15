ef6c90c6a0928e1acb60fdbae6cf569cc6938799
Mahesh <chmahesh.dev@gmail.com>
2025-08-15 13:32:18 +0530

    feat:hero-section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "M-ats" },
+    { name: "description", content: "Smart feed back for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-small.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Scores</h1>
+          <h2>Review submissions and get AI-powered feedback instantly.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => {
+              return <ResumeCard key={resume.id} resume={resume} />;
+            })}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
